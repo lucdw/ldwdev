38935f18d6afc6c604a87454cf1a2ea8349d2b18
lucdw <37792780+lucdw@users.noreply.github.com>
2024-03-27 12:36:14 +0100

    add compare_objects function

--- R/dev_msg.r
+++ R/dev_msg.r
@@ -1,37 +1,51 @@
-# Displays a message (... concatenated with spaces in between) without header
-# and formatted to have a maximum line length of 'txt.width'
+# Displays a message (... concatenated with spaces in between) with header
+# "ldwdev(function):" and formatted to have a maximum line length of 'txt.width'
 # while all but the first line start with 'indent' spaces
-dev_cat <- function(..., txt.width = 80L, indent = 4L) {
+dev_cat <- function(...) {
   wat <- unlist(list(...), use.names = FALSE)
-  cat(dev__msg(wat, "", txt.width, indent))
+  cat(dev_msg(wat))
 }
 
-# Displays a message with header 'ldwdev NOTE :' and formatted as
+# Displays a message formatted as
 # above via R function 'message'
-dev_note <- function(..., txt.width = 80L, indent = 4L) {
+dev_note <- function(...) {
   wat <- unlist(list(...), use.names = FALSE)
-  message(dev__msg(wat, gettext("NOTE"), txt.width, indent), domain = NA)
+  message(dev_msg(wat), domain = NA)
 }
 
-# Displays a message with header 'ldwdev WARNING :' and formatted as
+# Displays a message  formatted as
 # above via R function 'warning'
-dev_warn <- function(..., txt.width = 80L, indent = 4L) {
+dev_warn <- function(...) {
   wat <- unlist(list(...), use.names = FALSE)
-  warning(dev__msg(wat, gettext("WARNING"), txt.width, indent),
-          call. = FALSE, domain = NA)
+  warning(dev_msg(wat), call. = FALSE, domain = NA)
 }
 
-# Displays a message with header 'ldwdev ERROR :' and formatted as
+# Displays a message formatted as
 # above via R function 'stop'
-dev_stop <- function(..., txt.width = 80L, indent = 4L) {
+dev_stop <- function(...) {
   wat <- unlist(list(...), use.names = FALSE)
-  stop(dev__msg(wat, gettext("ERROR"), txt.width, indent),
-       call. = FALSE, domain = NA)
+  stop(dev_msg(wat), call. = FALSE, domain = NA)
 }
 
 # subroutine for above functions
-dev__msg <- function(wat, header = "", txt.width = 80L, indent = 2L) {
-  if (header != "") header <- paste("ldwdev", header, ":")
+dev_msg <- function(wat, txt.width = 80L, indent = 2L) {
+  x <- sub("[() ].*$", "", as.character(sys.calls()))
+  ignore.in.stack <- c(
+    "^eval$", "^try", "^doTryCatch", "^dev_msg", "^stop$", "^warn$",
+    "^which$", "^unique$", "^as\\.", "^unlist$", "^message$",
+    "^source$", "^withVisible$", "^tryCatch.W.E$", "^withCallingHandlers$",
+    "^do.call$"
+  )
+  ignores <- rep(FALSE, length(x))
+  for (ptrn in ignore.in.stack) {
+    ignores <- ignores | grepl(ptrn, x)
+  }
+  x <- x[!ignores]
+  if (length(x) == 0) {
+    header <- "ldwdev:"
+  } else {
+    header <- paste0("ldwdev(", x[length(x)], "):")
+  }
   # make sure we only have a single string
   txt <- paste(wat, collapse = " ")
   # split the txt in little chunks
@@ -61,4 +75,43 @@ dev__msg <- function(wat, header = "", txt.width = 80L, indent = 2L) {
     nstop <- nstart
   }
   paste(chunks, collapse = " ")
+}
+# Transforms a value to a character representation for use in messages
+# if logsep = "array" (default), letters[1:3] is transformed to ("a", "b", "c")
+# if logsep = "none", c("x", "y", "z") is transformed to "x", "y", "z"
+# if logsep = "and", 1:3 is transformed to 1, 2 and 3
+# if logsep = "or", c("a", "b", "c") is transformed to "a", "b" or "c"
+# The type of quote can be modified via parameter qd (default = TRUE).
+# If the object has names, the names will be prepended with a colon before the
+# value, e.g. c(x = 2.3, y = 4) --> (x : 2.3, y : 4).
+dev_msg_view <- function(x,
+                         log.sep = c("array", "none", "and", "or"),
+                         qd = TRUE) {
+  if (missing(x)) return("NULL")
+  log.sep <- match.arg(log.sep)
+  xn <- names(x)
+  if (is.list(x)) {
+    xx <- sapply(x, dev_msg_view)
+  } else {
+    if (is.character(x)) {
+      if (qd) {
+        xx <- dQuote(x, q = FALSE)
+      } else {
+        xx <- sQuote(x, q = FALSE)
+      }
+    } else {
+      xx <- as.character(x)
+    }
+    xx[is.na(x)] <- "NA"
+  }
+  if (!is.null(xn)) xx <- paste(xn, ":", xx)
+  if (length(xx) == 1) {
+    rv = xx
+  } else {
+    if (log.sep == "array") rv = paste0("(", paste(xx, collapse = ", "), ")")
+    if (log.sep == "none") rv = paste(xx, collapse = ", ")
+    if (log.sep == "and") rv = paste(paste(xx[-length(xx)], collapse = ", "), gettext("and"), xx[length(xx)])
+    if (log.sep == "or") rv = paste(paste(xx[-length(xx)], collapse = ", "), gettext("or"), xx[length(xx)])
+  }
+  rv
 }
